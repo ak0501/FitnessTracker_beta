e23d04263f41ebd273fb870de860d55eba2bbe33
khab0501 <48302535+khab0501@users.noreply.github.com>
2020-08-04 18:58:01 -0500

    defined file structure

---
+++ models/workout.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+
+    {
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+
+        exercises: [{
+            type: {
+                type: String,
+                trim: true,
+                required: "Exercise type is required"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Exrcise name is required"
+
+            },
+            duration: {
+                type: Number,
+                trim: true,
+                required: "Exercise duration is required"
+            },
+            weight: {
+                type: Number,
+                trim: true,
+                required: "number is Required"
+            },
+            reps: {
+                type: Number,
+                trim: true,
+                required: "number is Required"
+            },
+            sets: {
+                type: Number,
+                trim: true,
+                required: "number is Required"
+            }
+        }
+    ]
+    });
+
+
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -1,45 +1,33 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+import express, { urlencoded, json, static } from "express";
+import logger from "morgan";
+import { connect } from "mongoose";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-// const db = require("./models");
+// const db = require("./models/workout.js");
 
 const app = express();
 
 app.use(logger("dev"));
 
-app.use(express.urlencoded({
-    extended: true
-}));
-app.use(express.json());
+app.use(
+  urlencoded({
+    extended: true,
+  })
+);
+app.use(json());
 
-app.use(express.static("public"));
+app.use(static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", {
-    useNewUrlParser: true
+connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+  useNewUrlParser: true,
 });
 
-// db.Library.create({
-//         name: "Campus Library"
-//     })
-//     .then(dbLibrary => {
-//         console.log(dbLibrary);
-//     })
-//     .catch(({
-//         message
-//     }) => {
-//         console.log(message);
-//     });
-
-// app.post("/submit", ({
-//     body
-// }, res) => {
-//     db.Book.create(body)
-//         .then(({
+// app.post("/exercise", (req, res) => {
+//     db.create(req.body)
+//         .then(({s
 //             _id
-//         }) => db.Library.findOneAndUpdate({}, {
+//         }) => db.findOneAndUpdate({}, {
 //             $push: {
 //                 books: _id
 //             }
@@ -54,37 +42,17 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", {
 //         });
 // });
 
-// app.get("/books", (req, res) => {
-//     db.Book.find({})
-//         .then(dbBook => {
-//             res.json(dbBook);
-//         })
-//         .catch(err => {
-//             res.json(err);
-//         });
-// });
-
-// app.get("/library", (req, res) => {
-//     db.Library.find({})
-//         .then(dbLibrary => {
-//             res.json(dbLibrary);
-//         })
-//         .catch(err => {
-//             res.json(err);
-//         });
-// });
-
-// app.get("/populated", (req, res) => {
-//     db.Library.find({})
-//         .populate("books")
-//         .then(dbLibrary => {
-//             res.json(dbLibrary);
-//         })
-//         .catch(err => {
-//             res.json(err);
-//         });
-// });
+app.get("/exercise", (req, res) => {
+  console.log("we are here");
+  db.find({})
+    .then((dbFitness) => {
+      res.json(dbFitness);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
